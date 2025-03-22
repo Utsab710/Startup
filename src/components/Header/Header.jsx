aefba8a846405aea62f177b05ecba6330f22777f
Your Name <shresthautsab789@gmail.com>
2025-03-22 23:42:01 +0545

    Starting the startup website project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <section>
+        <div>
+          <div>
+            <img src="https://softechfoundation.com/upload_file/setting/1711949002_1167325993_1536148958_384808185_logo.png" />
+          </div>
+          <h1>About</h1>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Header;
